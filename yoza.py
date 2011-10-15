5eb589a2d8fce05b92c84417d1e843d2f343ed26
Jesse Determann <straightedgejesse@gmail.com>
2011-10-14 23:05:46 -0400

    Now totally Object Oriented

--- yoza.py
+++ yoza.py
@@ -5,13 +5,49 @@ import sys
 from subprocess import call
 import subprocess
 
-VER = "v0.3"
 class DisplayModule():
-    def __init__(self):
-        pass
+    def __init__(self, filename):
+        self.Git = GitManager(filename)
+        self.cur_frame = 0
+        self.max_frame = self.Git.fCount()
 
-    def get_version():
-        return VER
+    def nFrame(self):
+        self.cur_frame += 1
+        if not self.atMax():
+            return self.getCurFrame()
+        else: self.cur_frame -= 1
+
+    def pFrame(self):
+        self.cur_frame -= 1
+        if not self.atMin():
+            return self.getCurFrame()
+        else: self.cur_frame += 1
+
+    def getCurFrame(self):
+        return self.Git.getFrame(self.cur_frame)
+
+    def iFrame(self, index):
+        save = self.cur_frame
+        self.cur_frame = index
+        if not self.outOfRange():
+            return self.getCurFrame()
+        else: self.cur_frame = save
+
+    def outOfRange(self):
+        if self.atMax() or self.atMin(): return True
+        else: return False
+
+    def atMax(self):
+        if self.cur_frame >= self.max_frame: return True
+        else: return False
+        
+    def atMin(self):
+        if self.cur_frame < 0: return True
+        else: return False
+
+    def getFrameNum(self):
+        return self.cur_frame 
+
 #open_session()
 #close_session()
 #display()
@@ -20,9 +56,23 @@ class DisplayModule():
 #get_time()
 
 class DjangoInterface():
-    def __init__(self):
-        pass
+    def __init__(self, filename):
+        self.Player = DisplayModule(filename)
 
+    def nFrameButton(self):
+        return self.Player.nFrame()
+
+    def pFrameButton(self):
+        return self.Player.pFrame()
+
+    def jumpTo(self, index):
+        return self.Player.iFrame(index)
+
+    def checkFrameNum(self):
+        return self.Player.getFrameNum()
+
+    def refresh(self):
+        return self.Player.getCurFrame()
 #start_playback()
 #show_current_version()
 #pause()
@@ -34,9 +84,45 @@ class DjangoInterface():
 #measure_latency_between_classes()
 
 class GitManager():
-    def __init__(self):
-        pass
+    def __init__(self, filename):
+        self.fname = filename
+        hdir = "./.tralf/"
+        if not os.path.exists(hdir):
+            print "Error: No Tralf data directory found!"
+        self.dirname = hdir + "." + self.fname + "/"  #Check for .tralf/.<fiename>
+        os.chdir(self.dirname)
+        self.change_index = self.buildTable()
 
+    def buildTable(self):
+        #  Soooo. . . this shit here like, pulls up the git log and creates a local table:
+        #
+        #    ID|GIT HASH|YEAR-MONTH-DAY|HOUR:MINUTE:SECOND    #
+        #    --|--------|--------------|------------------    #
+        #
+        #   It's totally baller
+        log = subprocess.Popen(["git", "log", "--pretty=format:%H %ci"], stderr=subprocess.STDOUT, stdout=subprocess.PIPE)
+        logblob = log.stdout.read()
+        logdata = logblob.splitlines()
+        self.count = len(logdata)
+        print "-" + str(self.count) + " Entries-"
+        index = []
+        for i, datum in enumerate(logdata):
+            parsed = datum.split(" ")
+            record = (self.count - 1 - i, parsed[0], parsed[1], parsed[2])
+            index.append(record)
+        index.sort()      #Put them in order from Oldest to Newest, for funsies
+        print "#    ID|GIT HASH|YEAR-MONTH-DAY|HOUR:MINUTE:SECOND    #"
+        print "#    --|--------|--------------|------------------    #"
+        for entry in index:
+            print str(entry[0]) + " | " + entry[1] + " | " + entry[2] + " | " + entry[3]
+        return index
+
+    def getFrame(self, i):
+        frame = subprocess.Popen(["git", "show", str(self.change_index[i][1])+":"+self.fname], stderr=subprocess.STDOUT, stdout=subprocess.PIPE)
+        return frame.stdout.read()
+
+    def fCount(self):
+        return self.count
 #get_version()
 #checkout_repository()
 #read_forwards()
@@ -45,54 +131,27 @@ class GitManager():
 #get_time()
 
 
-#Here is some poorly organized code to open the file n shit
+#here in the main function we will create a DjangoInterface Object to run the sample.
+
 fname = sys.argv[1]
-hdir = "./.tralf/"
-if not os.path.exists(hdir):
-    print "Error: No Tralf data directory found!"
-dirname = hdir + "." + fname + "/"  #Check for .tralf/.<fiename>
-#check_init(dirname)
+Interface = DjangoInterface(fname)
 
-os.chdir(dirname)
-#  Soooo. . . this shit here like, pulls up the git log and creates a local table:
-#
-#    ID|GIT HASH|YEAR-MONTH-DAY|HOUR:MINUTE:SECOND    #
-#    --|--------|--------------|------------------    #
-#
-#   It's totally baller
-
-log = subprocess.Popen(["git", "log", "--pretty=format:%H %ci"], stderr=subprocess.STDOUT, stdout=subprocess.PIPE)
-logblob = log.stdout.read()
-logdata = logblob.splitlines()
-count = len(logdata)
-print "-" + str(count) + " Entries-"
-change_index = []
-for i, datum in enumerate(logdata):
-    parsed = datum.split(" ")
-    record = (count - 1 - i, parsed[0], parsed[1], parsed[2])
-    change_index.append(record)
-change_index.sort()      #Put them in order from Oldest to Newest, for funsies
-print "#    ID|GIT HASH|YEAR-MONTH-DAY|HOUR:MINUTE:SECOND    #"
-print "#    --|--------|--------------|------------------    #"
-for entry in change_index:
-    print str(entry[0]) + " | " + entry[1] + " | " + entry[2] + " | " + entry[3]
-#       logout = log.communicate()[0]
-#       print logout
-#       print count.communicate()
-#call(["git", "commit", "-am", '"Tralf History"'], stderr=subprocess.STDOUT, stdout=subprocess.PIPE)
+#This part will emulate an interface using the frame-by-frame style
 mode = 1
 u_input = 0
-i = 0
-while u_input != 'q' and i < count and i >= 0:
-    frame = subprocess.Popen(["git", "show", str(change_index[i][1])+":"+fname], stderr=subprocess.STDOUT, stdout=subprocess.PIPE)
-    print frame.stdout.read()
+print Interface.refresh()
+while u_input != 'q':
+    i = Interface.checkFrameNum()
     u_input = raw_input(fname+str(i) + ':')
 
     try:
         i = int(u_input)
+        print Interface.jumpTo(i)
     except:
         if u_input == 'l':
             mode *= -1
 
-        i += 1*mode
+        if mode > 0:  print Interface.nFrameButton()
+        else: print Interface.pFrameButton()
+
 os.chdir("../../")
