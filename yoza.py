505fa5c81990a06533b6f12ab0ab6cc3c035bccc
Jesse Determann <straightedgejesse@gmail.com>
2011-10-15 14:02:08 -0400

    Code clean-up and docstrings

--- yoza.py
+++ yoza.py
@@ -7,46 +7,66 @@ import subprocess
 
 class DisplayModule():
     def __init__(self, filename):
+        """Initiate Attributes"""
         self.Git = GitManager(filename)
         self.cur_frame = 0
         self.max_frame = self.Git.fCount()
 
     def nFrame(self):
+        """Advance to next History Frame
+        Returns Frame Record
+
+        """
         self.cur_frame += 1
-        if not self.atMax():
+        if not self._atMax():
             return self.getCurFrame()
         else: self.cur_frame -= 1
+            #Probably should throw an exception here
 
     def pFrame(self):
+        """Regress to previous History Frame
+        Returns Frame Record
+
+        """
         self.cur_frame -= 1
-        if not self.atMin():
+        if not self._atMin():
             return self.getCurFrame()
         else: self.cur_frame += 1
+            #Probably should throw an exception here
 
     def getCurFrame(self):
+        """Return current History Frame
+        Returns Frame Record
+
+        """
         return self.Git.getFrame(self.cur_frame)
 
     def iFrame(self, index):
+        """Return History Frame at index
+        Returns Frame Record
+
+        """
         save = self.cur_frame
         self.cur_frame = index
-        if not self.outOfRange():
+        if not self._outOfRange():
             return self.getCurFrame()
         else: self.cur_frame = save
-
-    def outOfRange(self):
-        if self.atMax() or self.atMin(): return True
-        else: return False
-
-    def atMax(self):
-        if self.cur_frame >= self.max_frame: return True
-        else: return False
-        
-    def atMin(self):
-        if self.cur_frame < 0: return True
-        else: return False
+            #Probably should throw an exception here
 
     def getFrameNum(self):
         return self.cur_frame 
+
+    def _outOfRange(self):
+        if self.atMax() or self.atMin(): return True
+        else: return False
+
+    def _atMax(self):
+        if self.cur_frame >= self.max_frame: return True
+        else: return False
+        
+    def _atMin(self):
+        if self.cur_frame < 0: return True
+        else: return False
 
 #open_session()
 #close_session()
@@ -85,22 +105,44 @@ class DjangoInterface():
 
 class GitManager():
     def __init__(self, filename):
+        """Initiate Attributes"""
         self.fname = filename
         hdir = "./.tralf/"
         if not os.path.exists(hdir):
             print "Error: No Tralf data directory found!"
         self.dirname = hdir + "." + self.fname + "/"  #Check for .tralf/.<fiename>
         os.chdir(self.dirname)
-        self.change_index = self.buildTable()
+        self.change_index = self._buildTable()
 
-    def buildTable(self):
-        #  Soooo. . . this shit here like, pulls up the git log and creates a local table:
+    def getFrame(self, i):
+        """Return History Frame at index i
+        Returns Frame Record
+
+        """
+        frame = subprocess.Popen(["git", "show",
+                                  str(self.change_index[i][1])+":"+self.fname],
+                                  stderr=subprocess.STDOUT,
+                                  stdout=subprocess.PIPE)
+        record = self.change_index[i]
+        linenum = record[4]
+        date = record[2]
+        time = record[3]
+        return [linenum, frame.stdout.read(), date, time]
+
+    def fCount(self):
+        """Returns number of History Frames"""
+        return self.count
+
+    def _buildTable(self):
+        # Soooo. . . this shit here like, pulls up the git log and creates
+        # a local table:
         #
-        #    ID|GIT HASH|YEAR-MONTH-DAY|HOUR:MINUTE:SECOND|CHANGE_LOC    #
-        #    --|--------|--------------|------------------|----------    #
+        #   ID|GIT HASH|YEAR-MONTH-DAY|HOUR:MINUTE:SECOND|CHANGE_LOC    #
+        #   --|--------|--------------|------------------|----------    #
         #
-        #   It's totally baller
-        log = subprocess.Popen(["git", "log", "--pretty=format:%H %ci"], stderr=subprocess.STDOUT, stdout=subprocess.PIPE)
+        #  It's totally baller
+        log = subprocess.Popen(["git", "log", "--pretty=format:%H %ci"],
+                               stderr=subprocess.STDOUT, stdout=subprocess.PIPE)
         logblob = log.stdout.read()
         logdata = logblob.splitlines()
         self.count = len(logdata)
@@ -114,29 +156,27 @@ class GitManager():
         for entry in index:
             i = entry[0]
             if i > 0:
-                linechange = subprocess.Popen(["git", "diff", "--unified=1", str(entry[1]), str(index[i-1][1])], stderr=subprocess.STDOUT, stdout=subprocess.PIPE)
-            else: linechange = subprocess.Popen(["git", "diff", "--unified=1", str(entry[1]), str(index[i+1][1])], stderr=subprocess.STDOUT, stdout=subprocess.PIPE)
+                linechange = subprocess.Popen(["git", "diff", "--unified=1",
+                                               str(entry[1]),
+                                               str(index[i-1][1])],
+                                               stderr=subprocess.STDOUT,
+                                               stdout=subprocess.PIPE)
+            else: linechange = subprocess.Popen(["git", "diff", "--unified=1",
+                                                 str(entry[1]),
+                                                 str(index[i+1][1])],
+                                                 stderr=subprocess.STDOUT,
+                                                 stdout=subprocess.PIPE)
             lc = linechange.stdout.read()
             try:
                 linenum =  int(lc.splitlines()[4][4:].split(",")[0]) + 1
             except: linenum = 0
             entry.append(linenum)
-        print "#    ID|GIT HASH|YEAR-MONTH-DAY|HOUR:MINUTE:SECOND|CHANGE_LOC    #"
-        print "#    --|--------|--------------|------------------|----------    #"
+        print "#  ID|GIT HASH|YEAR-MONTH-DAY|HOUR:MINUTE:SECOND|CHANGE_LOC  #"
+        print "#  --|--------|--------------|------------------|----------  #"
         for entry in index:
-            print str(entry[0]) + " | " + entry[1] + " | " + entry[2] + " | " + entry[3] + " | " + str(entry[4])
+            print (str(entry[0]) + " | " + entry[1] + " | " + entry[2] + " | " +
+                   entry[3] + " | " + str(entry[4]))
         return index
-
-    def getFrame(self, i):
-        frame = subprocess.Popen(["git", "show", str(self.change_index[i][1])+":"+self.fname], stderr=subprocess.STDOUT, stdout=subprocess.PIPE)
-        record = self.change_index[i]
-        linenum = record[4]
-        date = record[2]
-        time = record[3]
-        return [linenum, frame.stdout.read(), date, time]
-
-    def fCount(self):
-        return self.count
 #get_version()
 #checkout_repository()
 #read_forwards()
